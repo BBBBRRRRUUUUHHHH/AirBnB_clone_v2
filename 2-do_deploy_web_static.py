2e139f114484d86ce47ed88065533127764d1665
BBBBRRRRUUUUHHHH <brianhannington94@gmail.com>
2023-10-06 07:45:10 -0700

    task 2 pycodestyle

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
 Write a Fabric script (based on the file 1-pack_web_static.py)
-that distributes an archive to your web servers, using the function do_deploy
+that distributes an archive to your web servers, using the
+function do_deploy
 """
 
 from datetime import datetime
