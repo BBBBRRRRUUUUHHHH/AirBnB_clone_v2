3f896f8635dea7bc836239ce728839abad652982
Brian <125502769+BBBBRRRRUUUUHHHH@users.noreply.github.com>
2023-10-23 20:08:32 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -4,9 +4,8 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 import os
+from os import getenv
 
-
-<<<<<<< HEAD
 storage_type = getenv("HBNB_TYPE_STORAGE")
 
 class User(BaseModel, Base):
@@ -24,30 +23,3 @@ class User(BaseModel, Base):
         password = ''
         first_name = ''
         last_name = ''
-=======
-class User(BaseModel, Base):
-    """ user class"""
-    __tablename__ = 'users'
-    email = Column(
-        String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    password = Column(
-        String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    first_name = Column(
-        String(128), nullable=True
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    last_name = Column(
-        String(128), nullable=True
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    places = relationship(
-        'Place',
-        cascade="all, delete, delete-orphan",
-        backref='user'
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
-    reviews = relationship(
-        'Review',
-        cascade="all, delete, delete-orphan",
-        backref='user'
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
->>>>>>> ee6bc2a7072293464cf3777b828863e8745d746c
