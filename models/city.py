234c6911d7b80008e834ebab4af2735001f55bb3
Brian <125502769+BBBBRRRRUUUUHHHH@users.noreply.github.com>
2023-10-23 20:15:21 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -3,31 +3,18 @@
 import os
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-
+from os import getenv
 from models.base_model import BaseModel, Base
 
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 class City(BaseModel, Base):
-<<<<<<< HEAD
-        """ The city class, contains state ID and name """
-        __tablename__ = 'cities'
-        if storage_type == "db":
-            name = Column(String(128), nullable=False)
-            state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-            places = relationship('Place', backref='cities')
-        else:
-            name = ""
-            state_id = ""
-=======
-    """city class"""
+    """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    state_id = Column(
-        String(60), ForeignKey('states.id'), nullable=False
-        ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    name = Column(
-        String(128), nullable=False
-        ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    places = relationship(
-        'Place', cascade='all, delete, delete-orphan', backref='cities'
-        ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
->>>>>>> ee6bc2a7072293464cf3777b828863e8745d746c
+    if storage_type == "db":
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship('Place', cascade="all,delete", backref='cities')
+    else:
+        name = ""
+        state_id = ""
